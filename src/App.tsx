e2a52db727ebfcabdc11b33b950529ff54695486
fadayopaul <paulfadayo@yahoo.com>
2025-01-08 14:01:17 +0100

    add List component to App for displaying tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,14 @@
 import Form from "./components/Form";
+import List from "./components/List";
+import { type Task } from "./types/taskTypes";
+
+const Task: Task[] = [];
 
 function App() {
   return (
     <main>
       <Form />
+      <List />
     </main>
   );
 }
