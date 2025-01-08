a6f87fc81cecea70aad1bf344a0acfed40521526
fadayopaul <paulfadayo@yahoo.com>
2025-01-08 15:46:58 +0100

    implement List component: add task rendering and toggle functionality

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,5 +1,27 @@
-function List() {
-  return <div></div>;
+import { TaskType } from "../types/taskTypes";
+
+type ListProp = {
+  tasks: TaskType[];
+  toggleTask: (id: number) => void;
+};
+
+function List(props: ListProp) {
+  const { tasks, toggleTask } = props;
+  return (
+    <ul className="list">
+      {tasks.map((task: TaskType, index) => (
+        <li key={task.id}>
+          <p>{`${index + 1}.  ${task.description}`}</p>
+
+          <input
+            type="checkbox"
+            checked={task.isCompleted}
+            onChange={() => toggleTask(task.id)}
+          />
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default List;
