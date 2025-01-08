620eb07e4d1a438762c174f6540e78a311273f75
fadayopaul <paulfadayo@yahoo.com>
2025-01-08 15:46:44 +0100

    implement task management: add state for tasks, localStorage integration, and update Form and List components

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,40 @@
+import { useState } from "react";
 import Form from "./components/Form";
 import List from "./components/List";
-import { type Task } from "./types/taskTypes";
-
-const Task: Task[] = [];
+import { type TaskType } from "./types/taskTypes";
 
 function App() {
+  const fetchStorage = localStorage.getItem("tasks");
+  const parsedTasks = fetchStorage ? JSON.parse(fetchStorage) : [];
+
+  const [tasks, setTasks] = useState<TaskType[]>(parsedTasks);
+
+  const updateStorage = (tasks: TaskType[]) => {
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+  };
+
+  const addTask = (newTask: TaskType) => {
+    const updatedTask = [...tasks, newTask];
+    setTasks(updatedTask);
+
+    updateStorage(updatedTask);
+  };
+
+  const toggleTask = (id: number) => {
+    setTasks(
+      tasks.map((task) => {
+        if (task.id === id) {
+          return { ...task, isCompleted: !task.isCompleted };
+        }
+        return task;
+      })
+    );
+  };
+
   return (
     <main>
-      <Form />
-      <List />
+      <Form addTask={addTask} />
+      <List tasks={tasks} toggleTask={toggleTask} />
     </main>
   );
 }
