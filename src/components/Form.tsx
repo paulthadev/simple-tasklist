7b306b52558d998e7c82994c99bdbe8c8a94b240
fadayopaul <paulfadayo@yahoo.com>
2025-01-08 15:46:52 +0100

    enhance Form component: add task submission logic, input handling, and validation

--- src/components/Form.tsx
+++ src/components/Form.tsx
@@ -1,12 +1,47 @@
-function Form() {
+import { useState } from "react";
+import { TaskType } from "../types/taskTypes";
+
+interface TaskProps {
+  addTask: (task: TaskType) => void;
+}
+
+function Form({ addTask }: TaskProps) {
+  const [description, setDescription] = useState<string>("");
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+    e.preventDefault();
+
+    if (!description) {
+      alert("enter task");
+      return;
+    }
+
+    const task = {
+      id: Math.random(),
+      description: description,
+      isCompleted: false,
+    };
+
+    addTask(task);
+    setDescription("");
+  };
+
   return (
-    <div>
-      <form className="form">
+    <div className="form">
+      <form className="task-form">
         <input
           type="text"
           className="form-input"
-          placeholder="enter description"
+          placeholder="Enter description"
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
         />
+
+        <button className="btn" onClick={handleSubmit}>
+          Submit
+        </button>
       </form>
     </div>
   );
